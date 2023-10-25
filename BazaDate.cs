4b7d3e9ae8b1510f934209fd5a6fdd9287dd0eaa
sindilarstefan <sindilar.stefan@gmail.com>
2023-10-25 16:51:29 +0300

    1

---
+++ BazaDate.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ATM_Tiketing
+{
+    internal class BazaDate
+    {
+        private static BazaDate instance = null;
+        private SqlConnection con;
+
+        private BazaDate()
+        {
+            string constring = "Data Source=DESKTOP-6GJKG84; Initial Catalog=test_ts;Integrated Security=true";
+            con = new SqlConnection(constring);
+            try
+            {
+                con.Open();
+                Console.WriteLine("Conexiune cu baza de date reușită!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare: " + ex.Message);
+            }
+        }
+
+        public static BazaDate GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new BazaDate();
+            }
+            return instance;
+        }
+
+        public SqlConnection GetCon()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            else if (con.State == System.Data.ConnectionState.Broken)
+            {
+                con.Close();
+                con.Open();
+            }
+            return con;
+        }
+
+        public void CloseCon()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+    }
+}
---
+++ Window2.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ATM_Tiketing
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+        private void TiketsButton_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Content = null;
+            TabelTichete tabel = new TabelTichete();
+           // tabel.BackNavigationService = mainFrame.NavigationService;
+            mainFrame.NavigationService.Navigate(tabel);
+            //CreareContNou.Visibility = Visibility.Collapsed;
+        }
+        private void DelogareButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow wn = new MainWindow();
+            wn.Show();
+            this.Close();
+        }
+        private void Draw_Sin_Click(object sender, RoutedEventArgs e)
+        {
+            // Get canvas dimensions
+            //double canvasWidth = canvas.ActualWidth;
+            //double canvasHeight = canvas.ActualHeight;
+
+            //// Clear previous drawings on the canvas
+            //canvas.Children.Clear();
+
+            //// Define parameters for the sine wave
+            //double amplitude = 100;
+            //double frequency = 1;
+            //double phaseShift = 0;
+
+            //// Draw the sine wave on the canvas
+            //for (double x = 0; x < canvasWidth; x += 1)
+            //{
+            //    double y = amplitude * Math.Sin(2 * Math.PI * frequency * x / canvasWidth + phaseShift) + canvasHeight / 2;
+
+            //    // Draw a line segment for the sine wave
+            //    Line lineSegment = new Line
+            //    {
+            //        X1 = x,
+            //        Y1 = y,
+            //        X2 = x + 1,
+            //        Y2 = amplitude * Math.Sin(2 * Math.PI * frequency * (x + 1) / canvasWidth + phaseShift) + canvasHeight / 2,
+            //        Stroke = Brushes.Blue,
+            //        StrokeThickness = 1
+            //    };
+
+            //    // Add the line segment to the canvas
+            //    canvas.Children.Add(lineSegment);
+            //}
+        }
+
+    }
+}
