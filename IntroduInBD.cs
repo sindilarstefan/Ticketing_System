21ab95ed45c9b4dc61c732fac6db22e2add2b629
sindilarstefan <sindilar.stefan@gmail.com>
2023-11-02 13:41:00 +0200

    1

--- IntroduInBD.cs
+++ IntroduInBD.cs
@@ -6,72 +6,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Data.Common;
 
 namespace ATM_Tiketing
 {
     internal class IntroduInBD
     {
-        SqlConnection bd = BazaDate.GetInstance().GetCon();
-
-        public void set_bd(SqlConnection _bd)
-        {
-            bd = _bd;
-        }
+        BazaDate bd = BazaDate.GetInstance();
 
         public void insereazaUtilizator(string nume, string prenume, string mail, string nr_tel, string parola)
         {
             string query = $"INSERT INTO Utilizatori (nume, prenume, mail, nr_tel, parola) VALUES ('{nume}', " +
                 $"'{prenume}', '{mail}', '{nr_tel}', '{parola}')";
-            using (SqlConnection con = bd)
+
+            SqlConnection con = bd.getCon();
+            SqlCommand cmd = new SqlCommand(query, con);
+            int result = cmd.ExecuteNonQuery();
+
+            try
             {
-                try
+                if (result > 0)
                 {
-                    if (bd.State == System.Data.ConnectionState.Closed)
-                    {
-                        bd.Open();
-                    }
-
-                    SqlCommand cmd = new SqlCommand(query, con);
-                    int result = cmd.ExecuteNonQuery();
-                    if (result > 0)
-                    {
-                        Console.WriteLine("Datele au fost inserate cu succes.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Inserarea datelor a eșuat.");
-                    }
+                    Console.WriteLine("Datele au fost inserate cu succes.");
                 }
-                catch (Exception ex)
+                else
                 {
-                    Console.WriteLine("Error: " + ex.Message);
+                    Console.WriteLine("Inserarea datelor a eșuat.");
                 }
-
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            bd.closeCon();
+
         }
 
         public bool verificaUtilizator(string mail, string parola)
         {
             bool rezultatVerificare = false;
-            string query = "SELECT COUNT(*) FROM Utilizatori WHERE mail = @mail AND parola = @parola";
 
-            using (SqlCommand cmd = new SqlCommand(query, bd))
+            SqlConnection con = bd.getCon();
+            string query = "SELECT COUNT(*) FROM Utilizatori WHERE mail = @mail AND parola = @parola";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@mail", mail);
+            cmd.Parameters.AddWithValue("@parola", parola);
+
+            try
             {
-                cmd.Parameters.AddWithValue("@mail", mail);
-                cmd.Parameters.AddWithValue("@parola", parola);
-                try
+                int count = (int)cmd.ExecuteScalar();
+                if (count > 0)
                 {
-                    int count = (int)cmd.ExecuteScalar();
-                    if (count > 0)
-                    {
-                        rezultatVerificare = true;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error: " + ex.Message);
+                    rezultatVerificare = true;
                 }
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+            bd.closeCon();
+            
             return rezultatVerificare;
         }
 
